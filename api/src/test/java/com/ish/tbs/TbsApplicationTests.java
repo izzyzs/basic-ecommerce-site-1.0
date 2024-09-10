7835bc55c66d6dfe8dabc8f246d339d147df37a1
izzyzs <izsoum2024@gmail.com>
2024-09-09 23:49:45 -0400

    First commit

---
+++ api/src/test/java/com/ish/tbs/TbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ish.tbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
